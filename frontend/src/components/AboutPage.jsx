78d419c682c647e9b2e7672d40de3ad075351ee6
Monica Jin <jinliemail@gmail.com>
2026-07-21 07:46:19 -0400

    Update

--- frontend/src/components/AboutPage.jsx
+++ frontend/src/components/AboutPage.jsx
@@ -303,7 +303,7 @@ export default function AboutPage() {
             boxShadow: "0 2px 8px rgba(0,0,0,0.08)"
           }}
         >
-          <b>Ling Hao</b>
+          <b>Linghao Li</b>
           <br />
           PhD Student
           <br />
